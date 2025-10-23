6981a9837da2a49a3c589ec13e575c0ab1d162ec
raimu7 <k24089kk@aitech.ac.jp>
2025-10-23 15:29:59 +0900

    文字起こし更新

--- 02.py
+++ 02.py
@@ -23,9 +23,6 @@ def preprocess_audio(sound):
 
 audio_data = []
 
-# 音声データを音声ファイルから読み取る
-audio_data.append(AudioSegment.from_file("audio-output-before.wav", format="wav"))
-audio_data.append(AudioSegment.from_file("audio-output-after.wav", format="wav"))
 
 for data in audio_data:
     sound = preprocess_audio(data)
